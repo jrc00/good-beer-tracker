536a6dd4afd5e219dfdbabacdac517f96165bafe
Jeff Carter <jcarter00@gmail.com>
2016-03-24 12:25:08 -0400

    update views

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/beerDB');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use('/', router);
 
 app.set('views', './views');
@@ -19,4 +20,6 @@ app.set('view engine', 'ejs');
 
 app.listen(port);
 console.log('Server started on ' + port);
+
+
 
